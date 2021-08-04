763ea5e15d48d78cd2b79160b799b1632f2fd862
PremanandChowdhuy <freudigprem@gmail.com>
2021-08-04 11:25:32 +0530

    fix: Cleanup addded for every test

--- src/tests/Signup.test.js
+++ src/tests/Signup.test.js
@@ -1,8 +1,9 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 import Signup from '../components/signup/Signup'
 
 describe('Test the Signup UI component', () => {
   beforeEach(() => render(<Signup />))
+  afterEach(() => cleanup)
 
   it('Should have an input Element', () => {
     expect(screen.getByRole('textbox')).toBeInTheDocument()
@@ -14,9 +15,10 @@ describe('Test the Signup UI component', () => {
 })
 
 describe('Test the label', () => {
-  it('Should update the text entered in input', () => {
-    render(<Signup />)
+  beforeEach(() => render(<Signup />))
+  afterEach(() => cleanup)
 
+  it('Should update the text entered in input', () => {
     fireEvent.change(screen.getByRole('textbox'), {
       target: { value: 'test Input' },
     })
