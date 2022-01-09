6d12effcdab48fe502af44c3d8faa769fa54da65
PremanandChowdhury <freudigprem@gmail.com>
2022-01-09 21:58:48 +0530

    Adding the logic for the countdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css'
 import CountDown from './components/countDown/CountDown'
+// import CountDown2 from './components/countDown2/CountDown2'
+// import CountDown3 from './components/Countdown3'
 import Footer from './components/footer/Footer'
 
 const App = () => {
+  // const hoursMinSecs = { hours: 0, minutes: 0, seconds: 5 }
+
   return (
-    <div className="container">
+    <div className='container'>
       <CountDown />
+      {/* <CountDown2 hoursMinSecs={hoursMinSecs} /> */}
+      {/* <CountDown3 /> */}
       <Footer />
     </div>
   )
--- src/components/countDown/CountDown.js
+++ src/components/countDown/CountDown.js
@@ -6,9 +6,93 @@ const CountDown = () => {
   const [hr, setHr] = useState('')
   const [min, setMin] = useState('')
   const [sec, setSec] = useState('')
+  const [msg, setMsg] = useState('')
 
   let result =
     Number(day) * 86400 + Number(hr) * 3600 + Number(min) * 60 + Number(sec)
+
+  let timer = {
+    day,
+    hr,
+    min,
+    sec,
+  }
+  let intervalId = null
+
+  const startCountDown = () => {
+    intervalId = setInterval(() => {
+      if (
+        timer.day === '' &&
+        timer.hr === '' &&
+        timer.min === '' &&
+        timer.sec === ''
+      ) {
+        clearInterval(intervalId)
+        return
+      }
+      if (timer.hr <= 0 && timer.min <= 0 && timer.sec <= 0) {
+        // Day
+        timer.hr += 24
+        timer.min += 59
+        timer.sec += 59
+        setHr(timer.hr)
+        setMin(timer.min)
+        setSec(timer.sec)
+
+        if (timer.day > 0) {
+          timer.day -= 1
+          setDay(timer.day)
+          console.log('day:', timer.day)
+        }
+
+        if (timer.hr > 0) {
+          timer.hr -= 1
+          setHr(timer.hr)
+          console.log('hr:', timer.hr)
+        }
+
+        console.log('Min:', timer.min, '\n sec:', timer.sec)
+      }
+
+      if (timer.min <= 0 && timer.sec <= 0) {
+        // Hours
+        timer.min += 59
+        timer.sec += 59
+        setMin(timer.min)
+        setSec(timer.sec)
+
+        if (timer.hr > 0) {
+          timer.hr -= 1
+          setHr(timer.hr)
+          console.log('hr:', timer.hr)
+        }
+
+        console.log('Min:', timer.min, '\n sec:', timer.sec)
+      }
+
+      if (timer.min > 0 && timer.sec === 0) {
+        // Minutes
+        timer.min -= 1
+        setMin(timer.min)
+        timer.sec += 59
+        setSec(timer.sec)
+      }
+
+      if (timer.sec >= 0) {
+        // Seconds
+        timer.sec -= 1
+        if (timer.sec <= 0) {
+          timer.sec = 0
+        }
+        setSec(timer.sec)
+      }
+
+      if (timer.day < 0 && timer.hr < 0 && timer.min < 0 && timer.sec < 0) {
+        clearInterval(intervalId)
+        setMsg('Times Up!')
+      }
+    }, 5)
+  }
 
   const inputHandler = (e) => {
     const id = e.target.id
@@ -33,10 +117,10 @@ const CountDown = () => {
             type='number'
             id='day'
             value={day}
-            placeholder='00'
+            placeholder='0'
             onChange={inputHandler}
           />
-          <label htmlFor='day'>days</label>
+          <label htmlFor='day'>day</label>
         </div>
         <span> : </span>
         <div>
@@ -45,7 +129,7 @@ const CountDown = () => {
             name='hr'
             id='hr'
             value={hr}
-            placeholder='00'
+            placeholder='0'
             onChange={inputHandler}
           />
           <label htmlFor='hr'>hr</label>
@@ -57,7 +141,7 @@ const CountDown = () => {
             name='min'
             id='min'
             value={min}
-            placeholder='00'
+            placeholder='0'
             onChange={inputHandler}
           />
           <label htmlFor='min'>min</label>
@@ -69,7 +153,7 @@ const CountDown = () => {
             name='sec'
             id='sec'
             value={sec}
-            placeholder='00'
+            placeholder='0'
             onChange={inputHandler}
           />
           <label htmlFor='sec'>sec</label>
@@ -78,6 +162,9 @@ const CountDown = () => {
       <div className='result' data-testid='result'>
         {result}
       </div>
+      <button onClick={startCountDown}>Start</button>
+      <br />
+      <h2>{msg}</h2>
     </>
   )
 }
